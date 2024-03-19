d8f318070b08dbd37983bdc9c084b39fbed78e9a
Samuel García <Sam_Garca99@hotmail.com>
2024-03-18 21:55:55 -0600

    Se modifico la etiqueta .logo-container, y se agregó navbar-focus.js para mejor usabilidad

---
+++ scripts/navbar-focus.js
@@ -0,0 +1,9 @@
+
+const mainTag = document.getElementsByTagName('main');
+const topNav = document.querySelector('#myTopnav');
+
+mainTag[0].addEventListener('click',()=>{
+    // topNav.className = 'topnav';
+    console.log(mainTag);
+});
+
