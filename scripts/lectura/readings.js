a9723ac076eded81e79945e1b7600c89c557e472
Samuel García <Sam_Garca99@hotmail.com>
2024-04-01 00:07:01 -0600

    Se arreglaron los textos de Lectura:Page

--- scripts/lectura/readings.js
+++ scripts/lectura/readings.js
@@ -1,22 +1,22 @@
 const str1 = 
 "<p>Public transport is a convenient and affordable way for people to travel within cities and towns."+
-"It plays a crucial role in reducing traffic congestion, saving money, and benefiting the enviroment.</p>"+
+" It plays a crucial role in reducing traffic congestion, saving money, and benefiting the enviroment.</p>"+
 "<p>Firstly, public transport is cost-effective. It is usually much cheaper than owning and maintaining"+
-"a personal vehicle. By using buses, trams, or trains, commuters can save a significant amount of money"+
-"on fuel, parking, and maintenance expenses.</p>"+
+" a personal vehicle. By using buses, trams, or trains, commuters can save a significant amount of money"+
+" on fuel, parking, and maintenance expenses.</p>"+
 "<p>Secondly, it helps reduce traffic congestion. When more people use public transport, there are fewer"+
-"cars on the road. This leads to smoother traffic flow, shorter travel times, and less stress for everyone</p>"+
+" cars on the road. This leads to smoother traffic flow, shorter travel times, and less stress for everyone.</p>"+
 "<p>In addition to cost savings and reduced congestion, public transport is environmentally friendly. Buses"+
-"and trains produce fewer emissions per passenger compared to individual cars. This helps reduce air pollution"+
-"and contributes to a cleaner, healthier environment.</p>"+
+" and trains produce fewer emissions per passenger compared to individual cars. This helps reduce air pollution"+
+" and contributes to a cleaner, healthier environment.</p>"+
 "<p>However, there are some challenges associated with public transport. One common issue is the reliability"+
-"of schedules. Delays can occur due to traffic, weather, or technical problems, which can inconvenience passengers.</p>"+
+" of schedules. Delays can occur due to traffic, weather, or technical problems, which can inconvenience passengers.</p>"+
 "<p id='lastP'>Furthermore, some areas may have limited public transport options, making it difficult for people to access their"+
-"desired destinations conveniently.</p>";
+" desired destinations conveniently.</p>";
 
 const str2 = 
 "<p>Adoption is a compassionate and altruistic way of forming families. It is a process"+
-"where individuals or couples legally become the parents of a child who is not biologically"+
+" where individuals or couples legally become the parents of a child who is not biologically"+
 " theirs. This act of love and commitment is a heartfelt endeavor, and it plays a significant"+
 " role in the lives of countless children worldwide.</p>"+
 "<p>Firstly, adoption offers a chance for children to grow up in a stable and loving environment."+
@@ -28,7 +28,7 @@ const str2 =
 " supportive environment for a child. This scrutiny ensures the adopted child will be placed in a secure"+
 " and loving family setting.</p>"+
 "<p>In addition, there are different types of adoption. Open adoption involves ongoing contact between the"+
-"adoptive and birth parents, while closed adoption keeps identifying information confidential. International"+
+" adoptive and birth parents, while closed adoption keeps identifying information confidential. International"+
 " adoption is when children from one country are adopted by families from another, offering cultural diversity"+
 " and global connections.</p>"+
 "<p>Furthermore, adoption is not without its challenges. Some adopted children may face identity issues or a desire"+
@@ -38,14 +38,15 @@ const str2 =
 " and the satisfaction of providing a child with a loving family. It's a mutually beneficial arrangement, with love"+
 " and support at its core.</p>"+
 "On the other hand, adoption can be an emotionally challenging process. Waiting for the right match and going through"+
-" legal procedures can be stressful. However, the reward of bringing a child into a loving home far outweighs the difficulties</p>"+
+" legal procedures can be stressful. However, the reward of bringing a child into a loving home far outweighs the difficulties.</p>"+
 "<p id='lastP'>To sum up, adoption is a beautiful way to build families through love. It offers children the chance to have a better life,"+
 " provides loving parents the opportunity to fulfill their dreams of parenthood, and fosters a sense of unity and support within society.</p>";
 
 const str3 = 
 "<p>Animals, much like humans, navigate a spectrum of emotions, challenging preconceived notions about the depth of their"+
 " inner lives. Recent scientific endeavors explore the fascinating realm of animal emotions, shedding light on the rich tapestry"+
-" that defines their experiences.</p>";
+" that defines their experiences.</p>"+
+"<p>Consider"
 
 const readingsObj = {
     basico : {
