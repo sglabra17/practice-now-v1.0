1da744fbeef7e9c89fcd24859a44487b1f6cf3e7
Samuel García <Sam_Garca99@hotmail.com>
2024-04-11 23:13:33 -0600

    Se termino la funcionalidad principal del Sitio, Posibles Mejoras: Footer Options, Palabra del Día, Consumo API?

--- scripts/gramatica/nivel.js
+++ scripts/gramatica/nivel.js
@@ -1,19 +1,20 @@
 // predefined headers
 const levelHeaders = {
     'basico'    : [' - Nivel Básico',
-                   'Empieza a practicar inglés con ejercicios de gramática de nivel básico.',
-                  ['personal-pronouns','possessives','demonstrative-pronouns']
-                ],
+    'Empieza a practicar inglés con ejercicios de gramática de nivel básico.',
+    ['personal-pronouns','possessives','demonstrative-pronouns'] ],
 
     'intermedio': [' - Nivel Intermedio',
-    'Ejercicios para aquellos que dominan los conceptos básicos del lenguaje.'],
+    'Ejercicios para aquellos que dominan los conceptos básicos del lenguaje.',
+    ['indefinite-pronouns','relative-and-indefinite-pronouns','intensifiers-and-mitigators']],
 
     'avanzado'  : [' - Nivel Avanzado',
-    'Dirigido a usuarios avanzados, para los que quieren perfeccionar su inglés.']
+    'Dirigido a usuarios avanzados, para los que quieren perfeccionar su inglés.',
+    ['present-perfect','for/since/ago','modal-perfects']]
 };
 
 // get url value
-let nivel = location.search.split("=")[1];
+const nivel = location.search.split("=")[1];
 console.log(nivel);
 
 // HTML references - Headers
@@ -26,16 +27,22 @@ const qnsCtnr  = document.querySelector('#qns-ctnr');
 
 // Dynamic Titles
 title.innerHTML    += levelHeaders[nivel][0];
-subTitle.innerHTML =  levelHeaders[nivel][1];
+subTitle.innerHTML  = levelHeaders[nivel][1];
 
-// Initialize Theme
+// Grammar Topics Array
 const topicsArr = levelHeaders[nivel][2];
-let chosenTheme = 'personal-pronouns';
 
-// Initialize Questions
+// Initialize Theme & Questions
+let chosenTheme = levelHeaders[nivel][2][0];
 let questions   = qnsObj[nivel][chosenTheme];
 
-// create solve button
+// Create header
+const qnsHeader     = document.createElement('header');
+qnsHeader.id        = 'qns-hdr';
+qnsHeader.classList.add('formHeader');
+qnsHeader.innerText = 'Elige la respuesta correcta.';
+
+// Create Solve Button
 const btnSolve       = document.createElement('a');
 btnSolve.textContent = 'Resolver';
 btnSolve.href        = '#principal';
@@ -46,6 +53,7 @@ btnSolve.classList.add('button-solve');
 console.warn('Temas:');
 for (const theme of topicsArr) {
     const indxTheme    = topicsArr.indexOf(theme);
+    // Create Topic Button
     const topicBtn     = document.createElement('button');
     topicBtn.id        = `btn-${indxTheme}`;
     topicBtn.classList.add('reading-option');
@@ -53,7 +61,8 @@ for (const theme of topicsArr) {
     topicBtn.innerText = theme;
     optsCtnr.append(topicBtn);
 }
-// Topic Buttons List
+
+// Topic Buttons Array
 const topicBtnsArr = optsCtnr.children;
 
 // Select Grammar Topic
@@ -62,7 +71,7 @@ for (const button of topicBtnsArr) {
         chosenTheme = button.innerText;
         dynamiContent();
 
-        // get buttons id
+        // get buttons Identification
         const chipIndx = button.id.split('-')[1];
 
         // active / not active btn 
@@ -75,8 +84,6 @@ for (const button of topicBtnsArr) {
                 itm.className = 'reading-option rop-active';
             }
         }
-
-
     });
 }
 
@@ -85,7 +92,11 @@ const dynamiContent = () =>{
     questions = qnsObj[nivel][chosenTheme];
 
     // clear Container of Questions
-    qnsCtnr.innerHTML = ' <header id="qns-hdr" class="formHeader">Elige la respuesta correcta.</header>';
+    qnsCtnr.innerHTML   = '';
+
+    // append Header of Questions
+    qnsHeader.innerText = 'Elige la respuesta correcta.';
+    qnsCtnr.append(qnsHeader);
 
     for (const question of questions) {
         // Create <div> question container
@@ -112,26 +123,26 @@ const dynamiContent = () =>{
     
         // Create answers
         for (const op of question['options']) {
-            const indxOp = question['options'].indexOf(op);
+            const indxOp    = question['options'].indexOf(op);
     
             // create answer container
-            const awrCtnr = document.createElement('div');
+            const awrCtnr   = document.createElement('div');
             awrCtnr.classList.add('answer-ctnr');
             ansCtnr.append(awrCtnr);
     
             // create radio button
             const inRadio   = document.createElement('input');
             inRadio.type    = 'radio';
-            inRadio.id      = `${op}${question['num']}`;
+            inRadio.id      = `rad${indxOp}q${question['num']}`;
             inRadio.name    = `answer${question['num']}`;
             inRadio.value   = op;
             inRadio.checked = indxOp === 0 ? true : false;
             awrCtnr.append(inRadio);
     
             // create label
-            const radioLabel = document.createElement('label');
-            radioLabel.id    = `lbl-${question['num']}-${op}`;
-            radioLabel.setAttribute('for',`${op}${question['num']}`);
+            const radioLabel     = document.createElement('label');
+            radioLabel.id        = `lbl-${indxOp}-q${question['num']}`;
+            radioLabel.setAttribute('for',inRadio.id);
             radioLabel.innerText = op;
             awrCtnr.append(radioLabel);
         }
@@ -147,15 +158,20 @@ dynamiContent();
 // Solve Button - Event
 const qnsHdr = document.querySelector('#qns-hdr');
 btnSolve.addEventListener('click',()=>{
-    let correctas = 0;
-    qnsHdr.innerText = 'Resultado:';
+    let correctas    = 0;
+    qnsHdr.innerHTML = 'Resultado:';
+    console.log(qnsHdr.innerHTML);
 
     // Validate Answers
     for (const question of questions) {
         const pTag         = document.querySelector(`#qp${question['num']}`);
         const correct      = question['answer'];
-        const radioCorrect = document.querySelector(`#${correct}${question['num']}`);
-        const lblCorrect   = document.querySelector(`#lbl-${question['num']}-${correct}`);
+        const correctIndx  = question['options'].indexOf(correct);
+
+        const radioCorrect = document.querySelector(`#rad${correctIndx}q${question['num']}`);
+        const lblCorrect   = document.querySelector(`#lbl-${correctIndx}-q${question['num']}`);
+        // console.log('radioCorrect:',radioCorrect);
+        // console.log('lblCorrect:',lblCorrect);
         const isCorrect    = radioCorrect.checked;
 
         // Replace Blank Space
@@ -172,17 +188,19 @@ btnSolve.addEventListener('click',()=>{
         
         // disable radio btns, and <del> wrong asnwer
         for (const res of question['options']) {
+            const resIndx = question['options'].indexOf(res);
+
             // disable radio buttons - asnwr options
-            const rad    = document.querySelector(`#${res}${question['num']}`);
+            const rad    = document.querySelector(`#rad${resIndx}q${question['num']}`);
             rad.disabled = true;
             // <del> wrong answers
-            const lbl    = document.querySelector(`#lbl-${question['num']}-${res}`);
+            const lbl    = document.querySelector(`#lbl-${resIndx}-q${question['num']}`);
             if(!isCorrect){
                 lbl.innerHTML = rad.checked ? `<del>${res}</del>` : res;
             }
         }
 
-        // correct answer
+        // Style correct answer
         lblCorrect.classList.add('labelCorrect');
     }
 
@@ -194,7 +212,7 @@ btnSolve.addEventListener('click',()=>{
     const btnRetry       = document.createElement('a');
     btnRetry.textContent = 'Reintentar';
     btnRetry.classList.add('button-solve');
-    btnRetry.href = `gramatica.html?lvl=${nivel}`;
+    btnRetry.href        = `gramatica.html?lvl=${nivel}`;
     qnsCtnr.append(btnRetry);
 });
 
--- scripts/gramatica/questions.js
+++ scripts/gramatica/questions.js
@@ -66,23 +66,95 @@ const qnsObj = {
         'possessives':[
             {
                 num      : 1,
-                question : 'Who is Kate? Kate is my friend. ___ is a teacher.',
-                options  : ['Her','He','She','His'],
-                answer   : 'She'
+                question : 'This car is ___ car. This car is his.',
+                options  : ['he','her','his','hers'],
+                answer   : 'his'
+            },
+            {
+                num      : 2,
+                question : 'This house is our house. This house is ___.',
+                options  : ['our','ours','we','its'],
+                answer   : 'ours'
+            },
+            {
+                num      : 3,
+                question : 'This hat is my hat. This hat is ___.',
+                options  : ['I','me','mine','its'],
+                answer   : 'mine'
+            },
+            {
+                num      : 4,
+                question : 'This is your bike. This bike is ___.',
+                options  : ['you','your','yous','yours'],
+                answer   : 'yours'
+            },
+            {
+                num      : 5,
+                question : 'This is ___ dog. This dog is hers.',
+                options  : ['her','his','hers','she'],
+                answer   : 'her'
+            },
+            {
+                num      : 6,
+                question : 'These are ___ children. These children are theirs.',
+                options  : ['theirs','them','they','their'],
+                answer   : 'their'
             },
         ],
         'demonstrative-pronouns':[
             {
                 num      : 1,
-                question : 'Who is Kate? Kate is my friend. ___ is a teacher.',
-                options  : ['Her','He','She','His'],
-                answer   : 'She'
+                question : '___ is my car here.',
+                options  : ['This','That','These','Those'],
+                answer   : 'This'
+            },
+            {
+                num      : 2,
+                question : '___ are my children over there in the park.',
+                options  : ['This','That','These','Those'],
+                answer   : 'Those'
+            },
+            {
+                num      : 3,
+                question : '___ is my car over there.',
+                options  : ['This','That','These','Those'],
+                answer   : 'That'
+            },
+            {
+                num      : 4,
+                question : '___ are my children here.',
+                options  : ['This','That','These','Those'],
+                answer   : 'These'
+            },
+            {
+                num      : 5,
+                question : '___ bike over there is mine.',
+                options  : ['This','That','These','Those'],
+                answer   : 'That'
+            },
+            {
+                num      : 6,
+                question : 'Do you know ___ boys over there.',
+                options  : ['this','that','these','those'],
+                answer   : 'those'
+            },
+            {
+                num      : 7,
+                question : 'Can I use ___ pencil here?',
+                options  : ['this','that','these','those'],
+                answer   : 'this'
+            },
+            {
+                num      : 8,
+                question : 'I love ___ biscuits here.',
+                options  : ['this','that','these','those'],
+                answer   : 'these'
             },
         ]
     },
     
     intermedio: {
-        'tema1' :[
+        'indefinite-pronouns' :[
             {
                 num      : 1,
                 question : 'There is ___ in the bathroom at the moment.',
@@ -132,27 +204,105 @@ const qnsObj = {
                 answer   : 'Both'
             },
         ],
-        'tema2' :[
+        'relative-and-indefinite-pronouns' :[
             {
                 num      : 1,
-                question : 'There is ___ in the bathroom at the moment.',
-                options  : ['anybody','somebody','something','some'],
-                answer   : 'somebody'
+                question : 'I have a new job ___ pays very well.',
+                options  : ['where','that','who','whose'],
+                answer   : 'that'
+            },
+            {
+                num      : 2,
+                question : 'This is the school ___ my sister works.',
+                options  : ['that','which','who','where'],
+                answer   : 'where'
+            },
+            {
+                num      : 3,
+                question : 'There is ___ you can do.',
+                options  : ['nothing','anything','none','neither'],
+                answer   : 'nothing'
+            },
+            {
+                num      : 4,
+                question : 'Brian, ___ is my best student, is going to study in England next year.',
+                options  : ['whose','that','who','which'],
+                answer   : 'who'
+            },
+            {
+                num      : 5,
+                question : 'May I have ___ glass of water.',
+                options  : ['another','other','more','some'],
+                answer   : 'another'
+            },
+            {
+                num      : 6,
+                question : 'Is there ___ in the classroom?',
+                options  : ['none','somebody','anybody','someone'],
+                answer   : 'anybody'
+            },
+            {
+                num      : 7,
+                question : '___ is coming to the party',
+                options  : ['All','Everybody','You','Anybody'],
+                answer   : 'Everybody'
             },
         ],
-        'tema3' :[
+        'intensifiers-and-mitigators' :[
             {
                 num      : 1,
-                question : 'There is ___ in the bathroom at the moment.',
-                options  : ['anybody','somebody','something','some'],
-                answer   : 'somebody'
+                question : 'You are ___ the best friend a person could have.',
+                options  : ['much','by far','a great deal','highly'],
+                answer   : 'by far'
+            },
+            {
+                num      : 2,
+                question : 'There were some clouds, but overall it was a ___ nice day.',
+                options  : ['slightly','a bit','pretty','highly'],
+                answer   : 'pretty'
+            },
+            {
+                num      : 3,
+                question : 'The soup was ___ hoy. We had to wait 15 minutes before we could eat it.',
+                options  : ['extremely','amazingly','enough','absolutely'],
+                answer   : 'extremely'
+            },
+            {
+                num      : 4,
+                question : 'You are only ___ faster than me.',
+                options  : ['by far','easily','rather','slightly'],
+                answer   : 'slightly'
+            },
+            {
+                num      : 5,
+                question : 'Success is ___ probable.',
+                options  : ['seriously','really','highly','remarkably'],
+                answer   : 'highly'
+            },
+            {
+                num      : 6,
+                question : 'You are a ___ good friend.',
+                options  : ['terribly','very','highly','absolutely'],
+                answer   : 'very'
+            },
+            {
+                num      : 7,
+                question : 'One person was ___ injured in the accident.',
+                options  : ['amazingly','absolutely','totally','seriously'],
+                answer   : 'seriously'
+            },
+            {
+                num      : 8,
+                question : 'This gift is ___ perfect! I love it!',
+                options  : ['absolutely','very','slightly','highly'],
+                answer   : 'absolutely'
             },
         ]
     },
     
 
     avanzado : {
-        'tema1' :[
+        'present-perfect' :[
             {
                 num      : 1,
                 question : "I ___ walked in the door. Can I call you later?",
@@ -214,20 +364,122 @@ const qnsObj = {
                 answer   : 'already'
             },
         ],
-        'tema2' :[
+        'for/since/ago' :[
             {
                 num      : 1,
-                question : "I ___ walked in the door. Can I call you later?",
-                options  : ['already','just','still','yet'],
-                answer   : 'just'
+                question : "We have been working ___ 8 o'clock this morning.",
+                options  : ['for','since','ago'],
+                answer   : 'since'
+            },
+            {
+                num      : 2,
+                question : "They played football ___ three hours yesterday afternoon.",
+                options  : ['for','since','ago'],
+                answer   : 'for'
+            },
+            {
+                num      : 3,
+                question : "Neil has been sleeping ___ hours.",
+                options  : ['for','since','ago'],
+                answer   : 'for'
+            },
+            {
+                num      : 4,
+                question : "I learned to drive fifteen years ___.",
+                options  : ['for','since','ago'],
+                answer   : 'ago'
+            },
+            {
+                num      : 5,
+                question : "The children have been studying ___ this morning.",
+                options  : ['for','since','ago'],
+                answer   : 'since'
+            },
+            {
+                num      : 6,
+                question : "My parents went to New York City two years ___.",
+                options  : ['for','since','ago'],
+                answer   : 'ago'
+            },
+            {
+                num      : 7,
+                question : "I have been tired ___ last week.",
+                options  : ['for','since','ago'],
+                answer   : 'since'
+            },
+            {
+                num      : 8,
+                question : "She always talks on the phone ___ hours with her sister.",
+                options  : ['for','since','ago'],
+                answer   : 'for'
+            },
+            {
+                num      : 9,
+                question : "We have been attending the annual conference every year ___ 2005.",
+                options  : ['for','since','ago'],
+                answer   : 'since'
+            },
+            {
+                num      : 10,
+                question : "He has taught English at the academy ___ 8 years now.",
+                options  : ['for','since','ago'],
+                answer   : 'for'
             },
         ],
-        'tema3' :[
+        'modal-perfects' :[
             {
                 num      : 1,
-                question : "I ___ walked in the door. Can I call you later?",
-                options  : ['already','just','still','yet'],
-                answer   : 'just'
+                question : "You went to Moscow in February? It ___ have been cold!",
+                options  : ['must','could','can'],
+                answer   : 'must'
+            },
+            {
+                num      : 2,
+                question : "We ___ have stayed home. That movie was terrible!",
+                options  : ['will','must','should'],
+                answer   : 'should'
+            },
+            {
+                num      : 3,
+                question : "It ___ not have been the best cake you ever made, but it was still pretty good.",
+                options  : ['should','may','can'],
+                answer   : 'may'
+            },
+            {
+                num      : 4,
+                question : "It was bad, but it ___ have been worse!",
+                options  : ['could','will','must'],
+                answer   : 'could'
+            },
+            {
+                num      : 5,
+                question : "What a great day! I ___ not have asked for more.",
+                options  : ['must','could','may'],
+                answer   : 'could'
+            },
+            {
+                num      : 6,
+                question : "The dog just got out, it ___ have gone far.",
+                options  : ['must',"wouldn't have","can't have"],
+                answer   : "can't have"
+            },
+            {
+                num      : 7,
+                question : "You went to China? That ___ have been exciting!",
+                options  : ['may',"must","should"],
+                answer   : "must"
+            },
+            {
+                num      : 8,
+                question : "You ___ have brought a jacket. It's so cold tonight!",
+                options  : ['should',"may","will"],
+                answer   : "should"
+            },
+            {
+                num      : 9,
+                question : "If he had been in the competition he ___ have been the winner.",
+                options  : ['will',"might","ought to"],
+                answer   : "might"
             },
         ]
     }
